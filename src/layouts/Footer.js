acdd8d6c234b5be83aa47916d49a3bce10230eb6
Bartosz Maronka <bartek.maronka@gmail.com>
2020-03-06 14:40:26 +0100

    Create Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
         <div>
-            Footer
+            <h2>Footer</h2>
+            <Route path='/' exact render={() => (
+                <p>You are in <span>Home page</span></p>
+            )} />
+            <Route path='/:page' exact render={(props) => (
+                <p>You are in <span>{props.match.params.page}</span></p>
+            )} />
+            <Route path='/:page/:id' exact render={(props) => (
+                <p>You are in <span>{props.match.params.id}</span></p>
+            )} />
         </div>
     );
 }
